6dc510429bc01ca31d17c870924b22f9e990d486
sami sabir-idrissi <samisabiridrissi@icloud.com>
2024-11-05 18:47:31 -0500

    adding products

---
+++ src/api/products-shirts/controllers/products-shirts.ts
@@ -0,0 +1,7 @@
+/**
+ * products-shirts controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::products-shirts.products-shirts');
---
+++ src/api/products-shirts/routes/products-shirts.ts
@@ -0,0 +1,7 @@
+/**
+ * products-shirts router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::products-shirts.products-shirts');
